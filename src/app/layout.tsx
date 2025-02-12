e04791ab4d47cdd547ee2331d38da95aee0a118c
maredjokic <mare160696@gmail.com>
2025-02-12 19:58:12 +0100

    fix text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,25 +19,25 @@ export default function RootLayout(props: { children: React.ReactNode }) {
         <link rel="canonical" href="https://www.markodjokic.dev/" />
         <meta property="og:title" content="Portfolio CV Marko Đokić Software Engineer React .NET Vue.js Nuxt Next Dev" />
         <meta property="og:description" content="Portfolio CV Marko Đokić Software Engineer React .NET Vue.js Nuxt Next Dev Developer Frontend Backend" />
-        <meta property="og:image" content="https://www.slepsluzbaangel.com/_next/image?url=%2Fstatic%2Fimages%2Fimage2.png&w=2048&q=75" />
-        <meta property="og:url" content="https://www.slepsluzbaangel.com/" />
+        <meta property="og:image" content="https://www.markodjokic.dev/_next/image?url=%2Fstatic%2Fimages%2FmareImage.jpeg&w=384&q=75" />
+        <meta property="og:url" content="https://www.markodjokic.dev/" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Slep sluzba" />
-        <meta name="twitter:description" content="Slep sluzba Angel Pozarevac 069 660 620." />
-        <meta name="twitter:image" content="https://www.slepsluzbaangel.com/_next/image?url=%2Fstatic%2Fimages%2Fimage2.png&w=2048&q=75" />
+        <meta name="twitter:title" content="Portfolio CV Marko Đokić Software Engineer React .NET Vue.js Nuxt Next Dev" />
+        <meta name="twitter:description" content="Portfolio CV Marko Đokić Software Engineer React .NET Vue.js Nuxt Next Dev Developer Frontend Backend" />
+        <meta name="twitter:image" content="https://www.markodjokic.dev/_next/image?url=%2Fstatic%2Fimages%2FmareImage.jpeg&w=384&q=75" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
               "@context": "https://schema.org",
               "@type": "WebPage",
-              name: "Slep sluzba",
-              description: "Slep sluzba Angel Pozarevac 069 660 620.",
-              url: "https://www.slepsluzbaangel.com/",
-              telephone: "+38169660620",
+              name: "Marko Đokić Software Engineer",
+              description: "Portfolio CV Marko Đokić Software Engineer React .NET Vue.js Nuxt Next Dev",
+              url: "https://www.markodjokic.dev/",
+              telephone: "+381644402202",
               address: {
                 "@type": "PostalAddress",
-                "streetAddress": "8 marta 4A",
+                "streetAddress": "Požarevac",
                 "addressLocality": "Požarevac",
                 "postalCode": "12000",
                 "addressCountry": "RS"
