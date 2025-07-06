2d6205c460cb2ccdfd9ea3fd8fa04bead4a7c23a
Marko Djokic <mare160696@gmail.com>
2025-07-06 20:33:56 +0200

    download portfolio

--- src/components/ContactAddressBar.tsx
+++ src/components/ContactAddressBar.tsx
@@ -34,6 +34,12 @@ const theme = useTheme();
             <ListItemText primary="Linkedin" secondary="https://www.linkedin.com/in/djokicmarko/" />
             <ListItemText primary="Github" secondary="https://github.com/maredjokic/" />
             <ListItemText primary="Citizenship" secondary="🇷🇸  🇭🇺🇪🇺" secondaryTypographyProps={{ fontSize: '2rem' }}/>
+            <a
+                href="/static/files/CV-MarkoDjokic.pdf"
+                download
+              >
+                Download Portfolio
+            </a>
             <ListItemText primary="Location" />
          </Box>
         <Map />
